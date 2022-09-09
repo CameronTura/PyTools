e14717bab41b2cd848647609dad01ec2f81a77f2
Cameron Tura <89304138+KamuFeelsBad@users.noreply.github.com>
2022-09-09 17:47:12 +0100

    cleaner fizzbuzz

--- FIzzbuzz.py
+++ FIzzbuzz.py
@@ -1,1 +1,8 @@
-print ("\n".join(list(map(lambda i: "fizz"*(i%3==0)+ "buzz"*(i%5==0)or str(i), range(1, 101)))))
+
+def fizzbuzzer():
+    for i in range(1,101):
+        yield("fizz"*(i%3==0)
+              +"buzz"*(i%5==0)
+              or str(i))
+
+print("\n".join(list(fizzbuzzer())))
